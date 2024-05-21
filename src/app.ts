6b133792560595068f297a48dcb0f355d53ddb14
Muhammad Ibad <ibad3572@gmail.com>
2024-05-22 02:43:03 +0500

    add a router for express app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+import { userRouter } from "./routes/user.route";
+app.use("/api/v1/user", userRouter);
 
 export default app;
