cfcca008f6110509458b261fbf9ac15cc7e34267
Muhammad Ibad <ibad3572@gmail.com>
2024-05-28 02:36:50 +0500

    fixed

--- src/utils/generateTokens.ts
+++ src/utils/generateTokens.ts
@@ -6,15 +6,19 @@ interface Tokens {
     refreshToken: string;
 }
 const generateTokens = async (userId: string): Promise<Tokens> => {
-    const user = await User.findById({ _id: userId });
-    if (!user) {
-        throw new ApiError(401, "User not found | While generateTokens");
+    try {
+        const user = await User.findById({ _id: userId });
+        if (!user) {
+            throw new ApiError(401, "User not found | While generateTokens");
+        }
+        let refreshToken: string = user.generateRefreshToken();
+        let accessToken: string = user.generateAccessToken();
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false });
+        return { accessToken, refreshToken };
+    } catch (error) {
+        throw new ApiError(500, "Internal Server Error | While generateTokens");
     }
-    let refreshToken: string = user.generateRefreshToken();
-    let accessToken: string = user.generateAccessToken();
-    user.refreshToken = refreshToken;
-    await user.save({validateBeforeSave: false});
-    return { accessToken, refreshToken };
 };
 
 export { generateTokens };
