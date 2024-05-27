b60ba535eccf2668b7caeb51f0672583d7435209
Muhammad Ibad <ibad3572@gmail.com>
2024-05-27 15:56:10 +0500

    chore: Update config.ts to include additional environment variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,18 @@
-import { config as conf} from "dotenv"
+import { config as conf } from "dotenv";
 
-conf() // to load the .env file
-const _config={
-   PORT:process.env.PORT, 
-   MONGO_URI:process.env.MONGO_URI,
-   DB_NAME:"Library",
-   EMAIL_API:process.env.EMAIL_API,
-}
+conf(); // to load the .env file
+const _config = {
+    PORT: process.env.PORT,
+    MONGO_URI: process.env.MONGO_URI,
+    DB_NAME: "Library",
+    EMAIL_API: process.env.EMAIL_API,
+    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+};
 
-
-export const config= Object.freeze(_config)
+export const config = Object.freeze(_config);
 // to freez object so that it can not be modified(readonly)
